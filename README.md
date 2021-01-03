8b60b1db9bcc581715e1c0f2e868006bf3e641ec
Emerfoll <66702595+Emerfoll@users.noreply.github.com>
2021-01-03 12:38:25 -0600

    Added comments and a README.md file for readability.

---
+++ README.md
@@ -0,0 +1,8 @@
+Div Generator
+
+Clicking the "Generate" button creates a new div and increments the number displayed in the div.
+Each div created starts with a red background color, a "Yellow" button, a "Delete" button, and a number equal to the number of times the "Generate" button has been clicked.
+
+The "Yellow" button changes the background color from red to yellow.
+
+The "Delete" button removes the div it is in along with all the other elements within the div.
--- client.js
+++ client.js
@@ -1,37 +1,53 @@
+
+// log to confirm Javascript is sourced and working correctly.
 console.log('Javascript is ready');
 
 $(readyNow);
 
 function readyNow() {
-    
+    // log to confirm jQuery is sourced and working correctly.
     console.log('JQuery ready');
 
+    // click listeners to get the buttons ready for when they 
+    //  are clicked, regardless of when they are created.
     $('#generate').on('click', generate)
     $(document).on('click', '.yellowBtn', turnYellow)
     $(document).on('click', '.deleteBtn', deleteDiv)
-}
+} // end of readyNow
 
+// variable to keep track of how many times the generate button is clicked.
 let clickCounter = 0;
 
+// function that runs when the "Generate" button is clicked to create new divs.
 function generate() {
     console.log('Generate button clicked.');
+
+    // update the click count.
     clickCounter++;
+
+    // div that is created when "Generate" is clicked.
     $('#boxSection').append(`<div class="red"> 
                             <p>${clickCounter}</p>
                             <button class="yellowBtn">Yellow</button>
                             <button class="deleteBtn">Delete</button>
                             </div>`);
-}
+} // end of generate
+
 
 function turnYellow() {
     console.log('Yellow button clicked.');
+    // targets the div the button is located in and stores it in a variable.
     let thisDiv = $(this).parent();
-    $(thisDiv).toggleClass('red')
-    $(thisDiv).toggleClass('yellow')
 
-}
+    //  uses the div variable to toggle between the classes "red" and "yellow" 
+    // to change the background to the corresponding color.
+    $(thisDiv).removeClass('red')
+    $(thisDiv).addClass('yellow')
+
+} // end of turnYellow
 
 function deleteDiv() {
     console.log('Delete button clicked.');
+    // targets the div the button is located in and removes it.
     $(this).parent().remove();
-}
+} // end of deleteDiv
