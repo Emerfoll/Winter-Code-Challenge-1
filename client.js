357ef9c52d795de170ca9fb5eabd4fbe402b47c9
Emerfoll <66702595+Emerfoll@users.noreply.github.com>
2021-01-03 12:18:31 -0600

    buttons all work and yellow currently toggles between red and yellow.

--- client.js
+++ client.js
@@ -3,6 +3,35 @@ console.log('Javascript is ready');
 $(readyNow);
 
 function readyNow() {
-    console.log('JQuery ready');
     
+    console.log('JQuery ready');
+
+    $('#generate').on('click', generate)
+    $(document).on('click', '.yellowBtn', turnYellow)
+    $(document).on('click', '.deleteBtn', deleteDiv)
+}
+
+let clickCounter = 0;
+
+function generate() {
+    console.log('Generate button clicked.');
+    clickCounter++;
+    $('#boxSection').append(`<div class="red"> 
+                            <p>${clickCounter}</p>
+                            <button class="yellowBtn">Yellow</button>
+                            <button class="deleteBtn">Delete</button>
+                            </div>`);
+}
+
+function turnYellow() {
+    console.log('Yellow button clicked.');
+    let thisDiv = $(this).parent();
+    $(thisDiv).toggleClass('red')
+    $(thisDiv).toggleClass('yellow')
+
+}
+
+function deleteDiv() {
+    console.log('Delete button clicked.');
+    $(this).parent().remove();
 }
